05e8ef17686287962b5cf3e011d981d6a14d9bbf
Sayidabdullakhon <127374049+SadBaxodirov@users.noreply.github.com>
2023-11-08 23:54:04 +0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 
 #include <iostream>
 #include "task_1.h"
+#include "task_2.h"
 
 int main() {
     std::cout << "Task 1" << std::endl;
@@ -19,6 +20,22 @@ int main() {
     cout<<"The top value is "<<first.top()<<endl;
     std::cout << "Task 2" << std::endl;
     // call for task 2
+    int amount_n;
+    cin >> amount_n;
+    int n;
+    int count = 1;
+    for (int i=0;i<amount_n;i++){
+        cin>>n;
+        car.push(n);
+        if (n == count){count++; cout<<car.pop()<<endl;}
+        while (car.size() > 0){
+            if (car.front() == count){count++; cout<<car.pop()<<endl;}
+            else break;
+        }
+    }
+
+    if (car.size() == 0) {cout << "YES" << endl;}
+    else cout << "NO" << endl;
     std::cout << "Task 3" << std::endl;
     // call for task 3
     std::cout << "Task 4" << std::endl;
