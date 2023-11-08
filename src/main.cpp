f645bdc921b52b540620cff6ad20804fbcf66421
Sayidabdullakhon <127374049+SadBaxodirov@users.noreply.github.com>
2023-11-08 23:23:53 +0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,13 @@
 int main() {
     std::cout << "Task 1" << std::endl;
     // call for task 1
+    Stack first;
+    first.push(5);
+    first.push(6);
+    first.push(7);
+    cout<<"The top value is "<<first.top()<<endl;
+    first.pop();
+    cout<<"The top value is "<<first.top()<<endl;
     std::cout << "Task 2" << std::endl;
     // call for task 2
     std::cout << "Task 3" << std::endl;
