26f5c93c6f20af2ba94f7b0914f448e4dae2fe30
Sayidabdullakhon <127374049+SadBaxodirov@users.noreply.github.com>
2023-11-11 11:09:12 +0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,5 @@
-/*
- * Author:
- * Date:
- * Name:
- */
-
-#include <iostream>
 #include "task_1.h"
-#include "task_2.h"
-
 int main() {
-    std::cout << "Task 1" << std::endl;
-    // call for task 1
     Stack first;
     first.push(5);
     first.push(6);
@@ -18,29 +7,21 @@ int main() {
     cout<<"The top value is "<<first.top()<<endl;
     first.pop();
     cout<<"The top value is "<<first.top()<<endl;
-    std::cout << "Task 2" << std::endl;
-    // call for task 2
+    cout<<"Task 2";
+    Stack cars;
     int amount_n;
     cin >> amount_n;
     int n;
     int count = 1;
     for (int i=0;i<amount_n;i++){
         cin>>n;
-        car.push(n);
-        if (n == count){count++; cout<<car.pop()<<endl;}
-        while (car.size() > 0){
-            if (car.front() == count){count++; cout<<car.pop()<<endl;}
+        cars.push(n);
+        if (n==count){count++; cars.pop();}
+        while (cars.size()>0){
+            if (cars.top()==count){count++; cars.pop();}
             else break;
         }
     }
-
-    if (car.size() == 0) {cout << "YES" << endl;}
-    else cout << "NO" << endl;
-    std::cout << "Task 3" << std::endl;
-    // call for task 3
-    std::cout << "Task 4" << std::endl;
-    // call for task 4
-    std::cout << "Task 5" << std::endl;
-    // call for task 5
+    if (cars.size() !=0){cout<<0<<endl;}
     return 0;
 }
